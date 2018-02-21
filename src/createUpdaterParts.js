e872fa3a78d746b1d4e3f4e2d228e38ab94a8599
Warren Neil Gordon <neil@Warrens-MBP.home>
2018-02-21 11:46:25 -0500

    use $action to affect change, do not modify original action

--- src/createUpdaterParts.js
+++ src/createUpdaterParts.js
@@ -35,33 +35,34 @@ export default ({ store }) => {
 				// destructure action values used in processing
 				const { valueFunction, value, shouldDispatch, uiEventFunction, updateFunction, location, locationFunction, operation } = action
 
-
+				// create action to be processed
+				let $action = {}
 
 				// update value
-				action.value = valueFunction ? valueFunction({ error, res, store, value }) : value
+				$action.value = valueFunction ? valueFunction({ error, res, store, value }) : value
 
 				// update location
-				action.location = locationFunction ? locationFunction({ error, res, store, value }) : location
+				$action.location = locationFunction ? locationFunction({ error, res, store, value }) : location
 
 				// add type
-				action.type = action.location[0]
+				$action.type = action.location[0]
 
 				// trim first value from location
-				action.location.splice(0,1)
+				$action.location = action.location.slice(1)
 
 				// add name
-				action.name = actionName
+				$action.name = actionName
 
 				// add update function params
-				action.updateFunction = updateFunction ? updateFunction.bind(null, { res, error, store, fromJS, value }) : undefined
+				$action.updateFunction = updateFunction ? updateFunction.bind(null, { res, error, store, fromJS, value }) : undefined
 
 				// add operation
 				if (action.updateFunction) {
-					action.operation = 'updateIn'
+					$action.operation = 'updateIn'
 				} else if (!action.value) {
-					action.operation = 'deleteIn'
+					$action.operation = 'deleteIn'
 				} else {
-					action.operation = 'setIn'
+					$action.operation = 'setIn'
 				}
 
 				// TODO: add meta information about the updateSchemaCreator
@@ -70,7 +71,7 @@ export default ({ store }) => {
 				if ( shouldDispatch == undefined || shouldDispatch({ error, res, store, value }) ) {
 
 					// dispatch the action here
-					 store.dispatch(action)
+					 store.dispatch($action)
 
 					// fire ui event
 					if (uiEventFunction) uiEventFunction({ action, value, res, error, store, })
